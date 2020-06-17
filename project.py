5179b3ca17b865f45f97c03bbb1d9bc656008b8d
Eddie <66372048+Eddi-e@users.noreply.github.com>
2020-06-17 17:08:25 +0100

    Python code now works

--- project.py
+++ project.py
@@ -6,11 +6,14 @@ numCount = 2
 primeList = [2]
 numList = [2]
 numberLoop = True
-while numberloop is True:
-    nListLength = len[numList]
+while numberLoop is True:
+    primeTest = True
+    nListLength = len(numList)
     print (numList)
     numCount = numCount + 1
     if numCount % 2 != 0:
         primeCheck = True
     if primeCheck is True:
-        for i in range (0,nListLength)
+        for i in range (0,nListLength):
+            print (i)
+            print (numList[i])
